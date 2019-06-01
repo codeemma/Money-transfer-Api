798efe1885704ac16f91b82f7f9cc8fe7e562766
Emmanuel Olayinka <eakinolayinka@gmail.com>
2019-06-01 21:47:27 +0100

    create an endpoint for tranfering fund

---
+++ src/main/java/com/codeemma/revolut/endpoint/AccountEndpointHandler.java
@@ -0,0 +1,48 @@
+package com.codeemma.revolut.endpoint;
+
+import com.codeemma.revolut.account.AccountService;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountEndpointHandler {
+
+    private AccountService accountService;
+
+    public AccountEndpointHandler(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //URI - transfer?from={origin}&to={destination}&amount={amount}
+    public void handleTransfer(HttpExchange exchange) throws Exception{
+        Map<String, String> queryMap = getQueryMap(exchange);
+
+        String originatingAccountNumber = queryMap.get("from");
+        String destinationAccountNumber = queryMap.get("to");
+        BigDecimal amount =  new BigDecimal(queryMap.get("amount"));
+
+        accountService.transferFund(originatingAccountNumber, destinationAccountNumber, amount);
+
+        exchange.sendResponseHeaders(200,0);
+
+        exchange.close();
+
+    }
+
+    private Map<String, String> getQueryMap(HttpExchange exchange) {
+        String query = exchange.getRequestURI().getQuery();
+
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
+}
